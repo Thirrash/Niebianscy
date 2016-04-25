558f12a695cfaff8640f8eaf6c42e10ab0693895
Thirrash <thirrasch@gmail.com>
2016-04-25 22:47:20 +0200

    Basic highlight and gameobject click scripts
    Added:
                -Camera movement script during battle mode
                -New hex board
                -Basic highlight script
                -Basic statistic prefab

---
+++ Assets/Scripts/CameraBattleMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Script responsible for camera movement using WSAD keys during battle
+public class CameraBattleMovement : MonoBehaviour 
+{
+	public Transform MainCamera;
+	public float CameraSpeed;
+	public float ZoomSpeed;
+	private Vector3 CameraPosition;
+
+	void Start () 
+	{
+		CameraPosition = MainCamera.position;
+	}
+
+
+	void Update () 
+	{
+		//minuses here and there because axis vector direction is different; nothing to worry about
+		CameraPosition.x -= Input.GetAxis ("Vertical") * CameraSpeed;
+		CameraPosition.y -= Input.GetAxis ("Mouse ScrollWheel") * ZoomSpeed; 
+		CameraPosition.z += Input.GetAxis ("Horizontal") * CameraSpeed;
+
+		//camera limits
+		if (CameraPosition.x > 50.0f)	CameraPosition.x = 50.0f;
+		if (CameraPosition.x < 20.0f)	CameraPosition.x = 20.0f;
+		if (CameraPosition.y > 31.0f)	CameraPosition.y = 31.0f;
+		if (CameraPosition.y < 7.0f)	CameraPosition.y = 7.0f;
+		if (CameraPosition.z > 43.0f)	CameraPosition.z = 43.0f;
+		if (CameraPosition.z < 20.0f)	CameraPosition.z = 20.0f;
+
+		//save transform
+
+		MainCamera.position = CameraPosition;
+	}
+
+}
---
+++ Assets/Scripts/HighlightGameobject.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightGameobject : MonoBehaviour 
+{
+	public Camera MainCamera;
+	public GameObject ClickedObject, ClickedParentObject;
+	private GameObject HighlightedObject, HighlightedTemp, HighlightedParentObject;
+	private Material ObjectMat;
+	private Color tempColor;
+	private Renderer rend;
+	private Ray ray;
+	private RaycastHit hit;
+	private bool isMaterialChanged;
+
+	void Start() 
+	{
+		isMaterialChanged = false;
+	}
+
+	void Update()
+	{
+		ray = MainCamera.ScreenPointToRay (Input.mousePosition);
+
+		if (Physics.Raycast (ray, out hit, 100))
+		{
+			HighlightedObject = hit.transform.gameObject;
+			HighlightedParentObject = HighlightedObject.transform.parent.gameObject;
+			//Debug.Log ("Highlighted object - " + HighlightedParentObject.name + " / " + HighlightedObject.name);
+
+			if (isMaterialChanged) 
+			{
+				HighlightedTemp.GetComponent<Renderer>().material.color = tempColor;
+				isMaterialChanged = false;
+			}
+
+			if (HighlightedObject.tag == "Hex") 
+			{
+				isMaterialChanged = true;
+				rend = HighlightedObject.GetComponent<Renderer>();
+				ObjectMat = rend.material;
+				tempColor = ObjectMat.color;
+				ObjectMat.color = Color.red;
+			}
+
+			HighlightedTemp = HighlightedObject;
+		}
+
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			ClickedObject = HighlightedObject;
+			ClickedParentObject = HighlightedParentObject;
+			Debug.Log ("Click! " + ClickedObject);
+		}
+	}
+}
